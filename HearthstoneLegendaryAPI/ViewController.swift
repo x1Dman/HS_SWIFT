c5db375a5a38912a8ff2d4d277f90cd4ec88bde7
Nikita Khusnutdinov <a18579118@cab-wsm-0006665.sigma.sbrf.ru>
2020-07-29 12:53:39 +0300

    init app

---
+++ HearthstoneLegendaryAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HearthstoneLegendaryAPI
+//
+//  Created by 18579118 on 29.07.2020.
+//  Copyright © 2020 18579118. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
